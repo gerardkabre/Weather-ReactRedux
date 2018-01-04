d74f5910e72caae4f3745f470892489d5ec538a3
Gerard Cabrerizo <gerard.kabre@gmail.com>
2018-01-04 02:40:34 +0100

    adding weather-list container

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,7 @@ export function fetchWeather(city) {
     };
 } 
 
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ReduxPromise from "redux-promise";
 import App from "./components/app";
 import reducers from "./reducers";
 
+
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
